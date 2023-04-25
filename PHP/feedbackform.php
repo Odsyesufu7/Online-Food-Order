d994b2367eec95af1c0fedca6e86436db9124bd1
Odsyesufu7 <syesufu2000@yahoo.com>
2023-04-25 16:21:11 +0100

    Updated and saved

--- PHP/feedbackform.php
+++ PHP/feedbackform.php
@@ -269,7 +269,7 @@
 		<?php	
 		$c=1;
 		//Create a connection to the DB Server
-		$con=mysqli_connect('localhost','root','','Skychef Food');
+		$con=mysqli_connect('localhost','root','','Skychef');
 		$sql="Select * from feedback";
 		$res=mysqli_query($con,$sql);
 		while($row=mysqli_fetch_assoc($res)){
