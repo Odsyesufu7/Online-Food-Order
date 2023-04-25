bc17f350d457cfbc78feeaf45f0feabe770004da
Odsyesufu7 <syesufu2000@yahoo.com>
2023-04-25 16:41:49 +0100

    Updated and save

--- PHP/some.php
+++ PHP/some.php
@@ -4,9 +4,9 @@
 	extract($_GET);
 	setcookie('idlastused',$id, time()+36000000);
 	//Create a connection to the DB Server
-	$con=mysqli_connect('localhost','root','','foodicted');
+	$con=mysqli_connect('localhost','root','','Skychef');
 	$sql="Insert into feedback values ('$username','$dt','$txt',0,'','$id')"; // Insert values for all attributes
 	mysqli_query($con,$sql);
 	
-	header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . '/webtech_project/feedbkform.php');
+	header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . '/Skychef/feedbkform.php');
 ?>
