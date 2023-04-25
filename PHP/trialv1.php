a6f19cafa14be6cb0393bd908b2df85b6c40a7f7
Odsyesufu7 <syesufu2000@yahoo.com>
2023-04-25 16:47:28 +0100

    Updated and save

--- PHP/trialv1.php
+++ PHP/trialv1.php
@@ -3,18 +3,18 @@ session_start();
 extract($_GET);
 $email = $_GET['email'];
 $pwd = $_GET['password'];
-$con = mysqli_connect('localhost','root','','foodicted');
+$con = mysqli_connect('localhost','root','','Skychef');
 
 $sql ="SELECT * from detail WHERE email = '$email'";
 $res = mysqli_query($con, $sql);
 $row = mysqli_fetch_assoc($res);
 if($pwd == $row['password']){
 	$_SESSION['username']=$row['uname'];
-	echo "<script>window.location = \"http://localhost/webtech_project/home_pagev2.php\"</script>";
+	echo "<script>window.location = \"http://localhost/Skychef/home_pagev2.php\"</script>";
 }
 else{
 	echo "<script type='text/javascript'> alert('Invalid email or password. Try again!');</script>";
-	echo "<script>window.location = \"http://localhost/webtech_project/login.php\"</script>";
+	echo "<script>window.location = \"http://localhost/Skychef/login.php\"</script>";
 }
 ?>
 
