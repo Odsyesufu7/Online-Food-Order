c7966f296609b6583473a3a69267f1ba3fa1e5f5
Odsyesufu7 <syesufu2000@yahoo.com>
2023-04-25 16:43:53 +0100

    Updated and save

--- PHP/some2.php
+++ PHP/some2.php
@@ -2,8 +2,8 @@
 	extract($_GET);
 
 	//Create a connection to the DB Server
-	$con=mysqli_connect('localhost','root','','foodicted');
+	$con=mysqli_connect('localhost','root','','Skychef');
 	$sql="DELETE FROM feedback WHERE post='$status'";
 	mysqli_query($con,$sql);
-	header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . '/webtech_project/feedbkform.php');
+	header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . '/Skychef/feedbkform.php');
 ?>
