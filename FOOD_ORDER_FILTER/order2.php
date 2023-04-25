cd1da68aae1dd16eb189f59d1fc97f4ddf5be41f
Odsyesufu7 <syesufu2000@yahoo.com>
2023-04-25 13:24:15 +0100

    Updated and saved

--- FOOD_ORDER_FILTER/order2.php
+++ FOOD_ORDER_FILTER/order2.php
@@ -18,19 +18,19 @@ echo "<!DOCTYPE html>
 <br>
 <hr>
 <input type='checkbox' name='soup' value='Cream of Tomato soup' id='soup' > <label id='sl'>Cream of Tomato soup</label> 
-<label id='s_label'> Rs. 180/-</label>
+<label id='s_label'> $2.20/-</label>
 <input type='text' name='soup_qty' id='soup_qty' maxlength='1'><br>
 
 <input type='checkbox' name='starter' value='Paneer Tikka' id='starter'><label id='stl'>Paneer Tikka</label> 
-<label id='st_label'> Rs. 340/-</label>
+<label id='st_label'> $4.15/-</label>
 <input type='text' name='starter_qty' id='starter_qty'><br>
 
 <input type='checkbox' name='main-course' value='Corn Fried Rice' id='main-course'> <label id='mcl'>Corn Fried Rice </label>
-<label id='m_label'> Rs. 400/-</label>
+<label id='m_label'> $4.88/-</label>
 <input type='text' name='mc_qty' id='mc_qty'><br>
 
 <input type='checkbox' name='dessert' value='Gulab Jamoon' id='dessert'><label id='desl'> Gulab Jamoon</label>
-<label id='d_label'> Rs. 150/-</label>
+<label id='d_label'> $1.83/-</label>
 <input type='text' name='dessert_qty' id='dessert_qty' ><br>
 
 <input type='Submit' name='ORDER' id='sub' value='ORDER'> 
