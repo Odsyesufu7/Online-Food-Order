376f997fc11b4416d0715f0531da27e787a1d7ce
Odsyesufu7 <syesufu2000@yahoo.com>
2023-04-26 16:42:45 +0100

    Just update and saved README.md.txt

--- php/aboutus.php
+++ php/aboutus.php
@@ -5,7 +5,7 @@ session_start();
 <html>
 
   <head>
-    <title> About | Le Cafe' </title>
+    <title> About | Skychef Food' </title>
   </head>
 
   <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
@@ -49,7 +49,7 @@ session_start();
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Le Cafe'</a>
+          <a class="navbar-brand" href="index.php">Skychef Food'</a>
         </div>
 
         <div class="collapse navbar-collapse " id="myNavbar">
@@ -124,11 +124,11 @@ else {
     <div class="wide">
         
         <div class="tagline">It's not our <font color="red"><strong>work life</strong></font>, it's our <font color="green"><strong><em>life's work</em>.</strong></font></div>
-        <h3 style="color: red">About the food culture in Le cafe'</h3>
+        <h3 style="color: red">About the food culture in Skychef</h3>
         <br>
         <h3 style="color: white">Order food & beverages online from restaurants near & around you. <h3 style="color: white">We deliver food from your neighborhood local joints, your favorite cafes, luxurious & elite restaurants in your area,</h3><h3 style="color : white"> and also from chains like Dominos, KFC, Burger King, Pizza Hut, FreshMenu, Mc Donald's, Subway, Faasos, Cafe Coffee Day, Taco Bell, and more. Exciting bit?</h3><h3 style="color : white"> We place no minimum order restrictions! Order in as little (or as much) as you'd like. We'll Swiggy it to you!</h3></h3>
     </div>
 
-      <iframe src="https://www.google.com/maps/place/Le+Cafe/@11.9316155,79.8335933,17z/data=!3m1!4b1!4m5!3m4!1s0x3a53636a24873e45:0x7f1e7ad6a30982dd!8m2!3d11.9316103!4d79.835782" style="width: 250px"></iframe>
+      <iframe src="https://www.google.com/maps/place/Skychef/@11.9316155,79.8335933,17z/data=!3m1!4b1!4m5!3m4!1s0x3a53636a24873e45:0x7f1e7ad6a30982dd!8m2!3d11.9316103!4d79.835782" style="width: 250px"></iframe>
          </body>
 </html>
